e377ca65929906e0876bbdaec970be08053ba062
DevFinds <findsdev@gmail.com>
2024-02-24 23:20:38 +0300

    Добавил функции подключения выбранных стилей и скриптов

--- core/Render.php
+++ core/Render.php
@@ -45,7 +45,7 @@ class Render implements RenderInterface
         include_once $componentPath;
     }
 
-    public function enqueue_scripts()
+    public function enqueue_all_scripts()
     {
         // Передадим активную тему для системы в данную переменную
         $activeTheme = $this->config->get('app.theme', 'Basic');
@@ -56,7 +56,32 @@ class Render implements RenderInterface
         }
     }
 
-    public function enqueue_styles()
+    public function enqueue_selected_styles(array $styles_list = [])
+    {
+        // Передадим активную тему для системы в данную переменную
+        $activeTheme = $this->config->get('app.theme', 'Basic');
+        $themeName = $activeTheme . 'Theme';
+        $styles = array_diff(scandir(APP_PATH . "/public/assets/themes/$themeName/css"), array('..', '.'));
+        foreach ($styles_list as $style) {
+            if (in_array($style, $styles)) {
+                echo "<link rel='stylesheet' href='$style'>";
+            }
+        }
+    }
+
+    public function enqueue_selected_scripts(array $scripts_list = [])
+    {
+        $activeTheme = $this->config->get('app.theme', 'Basic');
+        $themeName = $activeTheme . 'Theme';
+        $scripts = array_diff(scandir(APP_PATH . "/public/assets/themes/$themeName/js"), array('..', '.'));
+        foreach ($scripts_list as $script) {
+            if (in_array($script, $scripts)) {
+                echo "<script src='$script'></script>";
+            }
+        }
+    }
+
+    public function enqueue_all_styles()
     {
         // Передадим активную тему для системы в данную переменную
         $activeTheme = $this->config->get('app.theme', 'Basic');
@@ -69,12 +94,12 @@ class Render implements RenderInterface
 
     public function system_head()
     {
-        $this->enqueue_styles();
+        $this->enqueue_all_styles();
     }
 
     public function system_footer()
     {
-        $this->enqueue_scripts();
+        $this->enqueue_all_scripts();
     }
 
     private function defaultData(): array
