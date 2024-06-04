facef7bfa01f89ef82079d8b8312af44b9f8162d
DevFinds <findsdev@gmail.com>
2024-06-04 22:00:59 +0300

    Починил страницу авторизации

--- themes/Basic/pages/login.php
+++ themes/Basic/pages/login.php
@@ -8,13 +8,13 @@
 ?>
 
 
-<?php $render->component('header') ?>
+<?php $render->component('header_without_navbar') ?>
 
 
 <div class="body-container">
     <div class="form-container">
         <div class="left-side">
-            <img src="./assets/img/logo_ShapeSider.svg" alt="logo">
+            <img src="./assets/img/logo_ShapeSider.svg" href="/" alt="logo">
             <p class="first-paragraph">Soft by DevFinds</p>
         </div>
 
@@ -27,55 +27,43 @@
                 <li class="icon-item"><a href=""><img src="assets\img\logo_telegram.svg" alt=""></a></li>
             </ul>
             <div class="right-container">
-                <form action="/register" method="post">
+                <form action="/login" method="post">
                     <div class="login-label_and_error">
                         <label for="login">Логин</label>
-                        <?php
-                        $data_to_check = [
-                            'login',
-                        ];
-
-                        foreach ($data_to_check as $data) {
-                            if ($session->has($data)) {
-
-                        ?>
-                                <ul class="error-list d-flex flex-column">
-                                    <?php foreach ($session->getFlash($data, 'nothing') as $error) {
-                                    ?>
-                                        <span class="login-error-message"><?php echo $error; ?></span>
-                            <?php  }
-                                }
-                            } ?>
-
                     </div>
-                    <input class="" type="text" id="login" name="login">
+                    <input class="" type="text" id="login" name="user_login">
                     <div class="login-label_and_error">
                         <label for="password">Пароль</label>
-                        <?php
-                        $data_to_check = [
-                            'password',
-                        ];
-
-                        foreach ($data_to_check as $data) {
-                            if ($session->has($data)) {
-
-                        ?>
-                                <ul class="error-list d-flex flex-column">
-                                    <?php foreach ($session->getFlash($data, 'nothing') as $error) {
-                                    ?>
-                                        <span class="login-error-message"><?php echo $error; ?></span>
-                            <?php  }
-                                }
-                            } ?>
-
                     </div>
-                    <input class="" type="password" id="password" name="password">
+                    <input class="" type="password" id="password" name="user_password">
                     <div class="register_double_btn_block">
                         <button type="submit" class="register-button" id="submitBtn">Войти</button>
                     </div>
                     <div class="auth-container">
-                        <p class="auth-paragraph">Уже есть аккаунт?</p>
-                        <a href="/login" class="auth-button">Войти</a>
+                        <p class="auth-paragraph">Нет аккаунта?</p>
+                        <a href="/register" class="auth-button">Регистрация</a>
+                    </div>
+                    <div class="error-container">
+
+                        <?php
+                        $data_to_check = [
+                            'user_login',
+                            'user_password'
+                        ];
+
+                        foreach ($data_to_check as $data) {
+                            if ($session->has($data)) {
+
+                        ?>
+                                <ul class="error-list d-flex flex-column">
+                                    <?php foreach ($session->getFlash($data, 'nothing') as $error) {
+                                    ?>
+                                        <li class="error-item btn btn-outline-danger my-1"><?php echo $error; ?></li>
+                            <?php  }
+                                }
+                            } ?>
+
+                                </ul>
                     </div>
                 </form>
             </div>
@@ -83,43 +71,5 @@
 
     </div>
 </div>
-
-
-
-<div class="login-form-container container p-3 flex-column d-flex text-center my-5">
-    <h1>Авторизация</h1>
-    <form action="/login" method="post" class="">
-        <label for="user_password" class="m-1 form-label">Логин</label>
-        <input type="text" name="user_login" placeholder="Ваш логин" class="form-control">
-        <label for="user_password" class="m-1 form-label">Пароль</label>
-        <input type="password" name="user_password" placeholder="Ваш пароль" class="form-control">
-
-        <button href="" type="submit" class="btn btn-primary my-4">Войти</button>
-
-    </form>
-    <div class="error-container">
-
-        <?php
-        $data_to_check = [
-            'user_login',
-            'user_password'
-        ];
-
-        foreach ($data_to_check as $data) {
-            if ($session->has($data)) {
-
-        ?>
-                <ul class="error-list d-flex flex-column">
-                    <?php foreach ($session->getFlash($data, 'nothing') as $error) {
-                    ?>
-                        <li class="error-item btn btn-outline-danger my-1"><?php echo $error; ?></li>
-            <?php  }
-                }
-            } ?>
-
-                </ul>
-    </div>
-</div>
-
 
 <?php $render->component('footer') ?>
