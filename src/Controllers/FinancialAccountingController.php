babb55990ad82ebc8deed20f75616a897bf2a538
Pace <p4ce99@gmail.com>
2024-12-06 19:56:37 +0300

    Вывод данных из бд

--- src/Controllers/FinancialAccountingController.php
+++ src/Controllers/FinancialAccountingController.php
@@ -4,11 +4,13 @@
 namespace Source\Controllers;
 
 use Core\Controller\Controller;
+use Source\Services\SupplierService;
 
 class FinancialAccountingController extends Controller
 {
     public function index()
     {
-        $this->render('/admin/dashboard/financial_accounting');
+        $supplierService = new SupplierService($this->getDatabase());
+        $this->render('/admin/dashboard/financial_accounting', ['suppliers' => $supplierService]);
     }
 }
