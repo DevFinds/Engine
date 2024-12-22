b808d4b230cd05022f08e31d1342d5d9177bfac9
OreeleS <strizhov.roman31@yandex.ru>
2024-12-22 19:14:24 +0300

    промежуточные

---
+++ themes/Carwashing/components/modals/attention_popup.php
@@ -0,0 +1,12 @@
+<div class="attention-popup">
+    <div class="attention-popup-content">
+        <div class="attention-popup-content-text">
+            <h2>Внимание!</h2>
+            <p>Вы действительно хотите удалить данную услугу?</p>    
+        </div>                        
+        <div class="attention-popup-content-buttons">
+            <button class="attention-popup-content-buttons-cancel">Отменить</button>
+            <button class="attention-popup-content-buttons-delete">Удалить</button>
+        </div>
+    </div>
+</div>
