1601c54b86fd16b7d79de872c2097e50a45cb986
pace99 <p4ce00@gmail.com>
2025-03-22 12:00:26 +0300

    Промежуточный

--- src/Controllers/ReportsController.php
+++ src/Controllers/ReportsController.php
@@ -1,44 +1,20 @@
 <?php
-
-
 namespace Source\Controllers;
 
 use Core\Controller\Controller;
 use Source\Services\EmployeeService;
-use Source\Services\ReportService;
 
 class ReportsController extends Controller
 {
     public function index()
     {
         $employeeService = new EmployeeService($this->getDatabase());
-        $this->createReport();
-        // $this->render('/admin/dashboard/reports', [
-        //     'employees' => $employeeService,
-        // ]);
-        
-    }
-
-    public function createReport()
-    {
-        $reportType = $_POST['reportType'] ?? '';
-        $filters = [
-            'warehouse_id' => 2,
-            'start_date' => '2023-06-01',
-            'end_date' => '2023-06-30'
-        ];
-
-         
-            $employeeService = new EmployeeService($this->getDatabase());
-            $reportDat = $employeeService->getAllFromDB();
-
-            $reportService = new ReportService($this->getDatabase());
-            $reportData = $reportService->generateProductReport($filters);  
-            
-
-        // $this->render('/admin/dashboard/reports', [
-        //     'reportData' => $reportData,
-        //     'reportType' => $reportType,
-        // ]);
+        $employees = $employeeService->getAllFromDB();
+        $employeeReports = $employeeService->generateEmployeeReport();
+        var_dump($employeeReports);
+        $this->render('/admin/dashboard/reports', [
+            'employees' => $employees,
+            'employeeReports' => $employeeReports
+        ]);
     }
 }
